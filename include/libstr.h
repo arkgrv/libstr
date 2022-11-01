019d811c5bb1dd326b35b38bac2942e23b018988
arkgrv <ag.novitskiy@icloud.com>
2022-11-01 03:30:18 +0100

    Added string constructor interface

--- include/libstr.h
+++ include/libstr.h
@@ -1,4 +1,25 @@
 #ifndef LIB_STRING_H
 #define LIB_STRING_H
 
+#include <stddef.h>
+
+/**
+ * This object represents a string.
+ * The implementation is hidden as it might
+ * change in the future.
+*/
+typedef struct string_t_ *string_t;
+
+/**
+ * Construct a new empty string.
+*/
+string_t new_string();
+
+/**
+ * Construct a new empty string with specific length.
+ * 
+ * @param length string pre-allocation length.
+*/
+string_t new_string_sz(size_t length);
+
 #endif
