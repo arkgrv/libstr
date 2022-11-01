725f0501d0114cf65924cd4fdcdbc684a378bda4
arkgrv <ag.novitskiy@icloud.com>
2022-11-01 04:07:24 +0100

    Added string with length constructor.

--- src/libstr.c
+++ src/libstr.c
@@ -1,6 +1,7 @@
 #include "libstr.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 #define SMALL_STR_BUF 25
 
@@ -22,14 +23,49 @@ struct string_t_ {
 */
 string_t new_string()
 {
-    // Allocate memory and just assign the small buffer to the
-    // data pointer.
-    
-    string_t str = malloc(sizeof(struct string_t_));
+    string_t str = (struct string_t_*) malloc(sizeof(struct string_t_));
     if (str == NULL)
         return NULL;
 
+    // Initialize small data
+    memset(str->small_data, 0, sizeof(str->small_data));
+    // Perform assignments
     str->data = str->small_data;
+    str->capacity = SMALL_STR_BUF;
+    str->length = 0;
+
+    return str;
+}
+
+/**
+ * Construct a new empty string with specific length.
+ * 
+ * @param length string pre-allocation length.
+ * @return new string if creation is successful,
+ * otherwise NULL.
+*/
+string_t new_string_sz(size_t length)
+{
+    const size_t cap = length + 1;
+
+    if (length < SMALL_STR_BUF)
+        return new_string();
+
+    string_t str = (struct string_t_*) malloc(sizeof(struct string_t_));
+    if (str == NULL)
+        return NULL;
+
+    // Setup everything
+    str->data = (char*) malloc(sizeof(char) * cap);
+    if (str->data == NULL) {
+        free(str);
+        return NULL;
+    }
+
+    memset(str->data, 0, sizeof(char) * cap);
+
+    str->capacity = cap;
+    str->length = 0;
 
     return str;
 }
