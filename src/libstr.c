d289019fda90daec86e853e53641af2c9f60a75a
arkgrv <ag.novitskiy@icloud.com>
2022-11-01 03:49:33 +0100

    Added implementation of empty string constructor

--- src/libstr.c
+++ src/libstr.c
@@ -1,5 +1,7 @@
 #include "libstr.h"
 
+#include <stdlib.h>
+
 #define SMALL_STR_BUF 25
 
 /**
@@ -12,4 +14,22 @@ struct string_t_ {
     char small_data[SMALL_STR_BUF];
 };
 
+/**
+ * Construct a new empty string.
+ * 
+ * @return new string if creation is successful,
+ * otherwise NULL.
+*/
+string_t new_string()
+{
+    // Allocate memory and just assign the small buffer to the
+    // data pointer.
+    
+    string_t str = malloc(sizeof(struct string_t_));
+    if (str == NULL)
+        return NULL;
 
+    str->data = str->small_data;
+
+    return str;
+}
