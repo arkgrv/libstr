d3f6b2f4833935118602bf1ae57a9ac0d775516b
arkgrv <ag.novitskiy@icloud.com>
2022-11-01 03:46:31 +0100

    Added struct implementation.

--- src/libstr.c
+++ src/libstr.c
@@ -1,1 +1,15 @@
 #include "libstr.h"
+
+#define SMALL_STR_BUF 25
+
+/**
+ * Implementation of string struct
+*/
+struct string_t_ {
+    size_t length;
+    size_t capacity;
+    char *data;
+    char small_data[SMALL_STR_BUF];
+};
+
+
